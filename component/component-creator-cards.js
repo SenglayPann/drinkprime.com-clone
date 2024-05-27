ecba26aca69bc7df9e1783d5ab46de70c8da445e
Chhunkea <kimchhunkea64@gmail.com>
2024-05-27 18:26:22 +0700

    Merge: merge creator component

--- component/component-creator-cards.js
+++ component/component-creator-cards.js
@@ -49,11 +49,11 @@ document.addEventListener("DOMContentLoaded", function() {
             const creatorCard = document.createElement("div");
             creatorCard.classList.add("relative");
             creatorCard.innerHTML = `
-                 <img src="${creator.imgSrc}" alt="creator image" class="object-contain w-full h-auto">
-                 <div class="text-left des:text-white p-[15px] mb-[20px] des:absolute des:bottom-0 des:left-0 des:w-full text-stroke-1">
-                     <h3 class="font-normalidadExteded font-extrabold text-lg mb-[5px] tracking-[0.2em]">${creator.name}</h3>
-                    <h3 class="font-normalidadExteded text-base  tracking-[1.47px]">${creator.location}</h3>
-                    <p class="font-normalidadExteded mt-[12px] mb-[10px] text-sm des:text-xs underline des:no-underline underline-offset-3 des:border-[1px] des:bg-black des:mt-[16px] des:px-[12px] des:py-[10px] w-[140px] tracking-[1.5px]">LEARN MORE</p>
+                 <img src="${creator.imgSrc}" alt="creator image" class="object-cover w-full h-[450px]">
+                 <div class="text-left tab1:text-black p-[15px] mb-[20px] tab1:absolute tab1:bottom-0 tab1:left-0 tab1:w-full text-stroke-1">
+                    <h3 class="font-normalidadExteded tab1:text-white tab1:drop-shadow-[2px_2px_4px_black] font-extrabold text-lg mb-[5px] tracking-[0.2em]">${creator.name}</h3>
+                    <h3 class="font-normalidadExteded tab1:text-white tab1:drop-shadow-[2px_2px_4px_black] text-base  tracking-[1.47px]">${creator.location}</h3>
+                    <p class="font-normalidadExteded mt-[12px] mb-[10px] text-sm tab1:text-xs underline tab1:no-underline underline-offset-3 tab1:border-[1px] tab1:border-black tab1:bg-white tab1:mt-[16px] tab1:px-[12px] tab1:py-[10px] w-[140px] tracking-[1.5px]">LEARN MORE</p>
                  </div>`;
 
             creatorGrid.appendChild(creatorCard);
